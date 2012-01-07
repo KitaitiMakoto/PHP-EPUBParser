0efff1354458c605aba5464b48c7dbe1ad97e5ac
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-01-08 07:11:33 +0900

    Change namespace in Main.php

--- src/EPUBParser/Main.php
+++ src/EPUBParser/Main.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * PEAR2\EPUBParser\Main
+ * EPUBParser\Main
  *
  * PHP version 5
  *
  * @category  Yourcategory
- * @package   PEAR2_EPUBParser
+ * @package   EPUBParser
  * @author    Your Name <handle@php.net>
  * @copyright 2012 Your Name
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
@@ -17,13 +17,13 @@
  * Main class for PEAR2_EPUBParser
  *
  * @category  Yourcategory
- * @package   PEAR2_EPUBParser
+ * @package   EPUBParser
  * @author    Your Name <handle@php.net>
  * @copyright 2012 Your Name
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @link      http://svn.php.net/repository/pear2/PEAR2_EPUBParser
  */
-namespace PEAR2\EPUBParser;
+namespace EPUBParser;
 class Main
 {
 }
