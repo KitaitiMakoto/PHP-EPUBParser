8c4ec688314161e7de608235ef358abce928cbd9
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-01-08 23:36:10 +0900

    [Package\Manifest\Item]Add followFallbackUntil()

--- src/EPUBParser/EPUB/Publication/Package/Manifest/Item.php
+++ src/EPUBParser/EPUB/Publication/Package/Manifest/Item.php
@@ -8,6 +8,8 @@ class Item
     protected $_href;
     protected $_mediaType;
     protected $_fallback;
+    protected $_properties;
+    protected $_mediaOverlay;
     protected $_IRI;
 
     /**
@@ -19,7 +21,7 @@ class Item
         if (! $dir = realpath($baseDirectory)) {
             throw new \RuntimeException("Directory not exist: $baseDirectory");
         }
-        foreach (array('id', 'href', 'mediaType') as $name) {
+        foreach (array('id', 'href', 'mediaType', 'properties', 'mediaOverlay') as $name) {
             if (isset($vars[$name])) {
                 $this->{'_' . $name} = $vars[$name];
             }
@@ -51,6 +53,16 @@ class Item
         return $this->_fallback;
     }
 
+    public function getProperties()
+    {
+        return $this->_properties;
+    }
+
+    public function getMediaOvelay()
+    {
+        return $this->_mediaOverlay;
+    }
+
     public function _setFallback(Manifest\Item $fallback)
     {
         $this->_fallback = $fallback;
@@ -60,6 +72,26 @@ class Item
     public function getIRI()
     {
         return $this->_IRI;
+    }
+
+    /**
+     * To do: Check recursive fallback chain
+     * To do: Accept SplDoublyLinkedList and check not using in_array
+     *
+     * @param string|array $mediaTypes Media type like 'application/xhtml+xml' or array('image/png', 'image/jpeg')
+     */
+    public function followFallbackUntil($mediaTypes)
+    {
+        if (! is_array($mediaTypes)) {
+            $mediaTypes = array($mediaTypes);
+        }
+        if (in_array($this->_mediaType, $mediaTypes)) {
+            return $this;
+        }
+        if (is_null($this->_fallback)) {
+            return null;
+        }
+        return $this->_fallback->followFallbackUntil($mediaTypes);
     }
 
     protected function _setIRI($basedir)
