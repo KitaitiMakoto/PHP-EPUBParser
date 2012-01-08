a79f87a3f96d93c869dd2565af95c2897c8445d3
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-01-08 23:39:15 +0900

    [Book]Implement __construct() and two getters

--- src/EPUBParser/EPUB/Book.php
+++ src/EPUBParser/EPUB/Book.php
@@ -1,6 +1,32 @@
 <?php
 namespace EPUBParser\EPUB;
+use EPUBParser\EPUB;
 
 class Book
 {
+    protected $_container;
+    protected $_package;
+
+    public function __construct($directory)
+    {
+        if (! $directory = realpath($directory)) {
+            throw new \Exception("Directory not exist: $directory");
+        }
+
+        $container = $directory . '/' . EPUB\OCF\Container::FILEPATH;
+        $this->_container = EPUB\OCF\Container::parse($container);
+
+        $package = $directory . '/' . $this->_container->getPrimaryRootfile();
+        $this->_package = EPUB\Publication\Package::parse($package);
+    }
+
+    public function getContainer()
+    {
+        return $this->_container;
+    }
+
+    public function getPackage()
+    {
+        return $this->_package;
+    }
 }
